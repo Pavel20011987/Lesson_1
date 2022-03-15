537a94e758acc66735c97ae956a34a5ef1d12538
Pavel20011987 <ivanovpaul2008@rambler.ru>
2022-03-15 22:51:34 +0300

    Homework_2_lesson2

---
+++ Lesson_2/Lesson2_3.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson_2
+{
+    class Lesson2_3
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите четное или нечетное число:");
+
+
+            byte num = byte.Parse(Console.ReadLine());
+
+            if (num % 2 == 0)
+            {
+                Console.WriteLine("Четное число");
+            }
+
+
+            else
+            {
+                Console.WriteLine("Нечетное число");
+            }
+
+        }
+    }
+}
