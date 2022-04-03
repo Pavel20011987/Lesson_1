6d80d7ad4e0ec436e80cde94a512c2ef3161fad9
Pavel20011987 <ivanovpaul2008@rambler.ru>
2022-04-03 09:25:10 +0300

    all_commit

---
+++ Lesson_4/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            getfullname("Петров", "Валерий", "Валерьевич");
+            getfullname("Сидоров", "Вадимир", "Иванович");
+            getfullname("Саврасов", "Петр", "Евгеньевич");// вызов метода в методе main
+        }
+        static void getfullname(string lastname, string firstname, string patronymic)// метод getfullname с аргументами и выводящая в консоль значения аргументов
+        {
+            Console.WriteLine($"{lastname} {firstname} {patronymic}");
+        }
+    
+    
+    }
+}
