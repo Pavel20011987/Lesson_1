84a4fa0557dd28aac383a6c00210843c3ef7399c
Pavel20011987 <ivanovpaul2008@rambler.ru>
2022-04-11 02:11:11 +0300

    commit_hw_7

--- Lesson_7/Program.cs
+++ Lesson_7/Program.cs
@@ -10,6 +10,13 @@ namespace Lesson_7
     {
         static void Main(string[] args)
         {
+            string secret = "some secret password";
+            Console.WriteLine("Enter password:");
+            string input = Console.ReadLine();
+            if (input == secret)
+            {
+                Console.WriteLine("Welcome!");
+            }
         }
     }
 }
